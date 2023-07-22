83b220220dc53130ec394209ce34f271088725b5
Shogo Kitada <contact@shogo0x2e.com>
2023-07-22 05:22:32 +0000

    spring を導入
    https://start.spring.io/ を使った。

---
+++ src/main/java/com/shogo0x2e/oop1/chatService/Oop1ChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shogo0x2e.oop1.chatService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop1ChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oop1ChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shogo0x2e/oop1/chatService/Oop1ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shogo0x2e.oop1.chatService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop1ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
