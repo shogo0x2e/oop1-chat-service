8fb70326192d950f736b445399702e59924ce891
Shogo Kitada <contact@shogo0x2e.com>
2023-07-22 04:55:31 +0000

    maven で環境作った

---
+++ src/main/java/com/shogo0x2e/oop1/chatService/App.java
@@ -0,0 +1,13 @@
+package com.shogo0x2e.oop1.chatService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
