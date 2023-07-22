3d1b09dcbe9c78ae5ee400a1af9ec23e08fe86ad
Shogo Kitada <contact@shogo0x2e.com>
2023-07-22 05:28:32 +0000

    課題の条件に合わせて Main.java に変更

--- src/main/java/com/shogo0x2e/oop1/chatService/Main.java
+++ src/main/java/com/shogo0x2e/oop1/chatService/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oop1ChatServiceApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Oop1ChatServiceApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
